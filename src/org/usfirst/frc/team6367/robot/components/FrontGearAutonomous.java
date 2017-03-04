060abc7ce9ddb87cac639fedaa9ab8f3c963f965
HKenfack <harold.kenfack@ebrooke.org>
2017-03-04 17:04:48 -0500

    Created FrontGearAutonomous

---
+++ src/org/usfirst/frc/team6367/robot/components/FrontGearAutonomous.java
@@ -0,0 +1,35 @@
+package org.usfirst.frc.team6367.robot.components;
+
+import org.usfirst.frc.team6367.robot.components.DriveTrain;
+
+import io.github.robotpy.magicbot.sm.TimedState;
+
+public class FrontGearAutonomous {
+
+	DriveTrain driveTrain;
+	
+	@TimedState(first=true, duration=2)
+	public void drivingForward(){
+			   driveTrain.move(-0.5, 0);
+		
+	}
+	@TimedState( duration=1)
+	public void Wait(){
+		driveTrain.move(0, 0);
+		
+	}
+	@TimedState(duration=1)
+	public void moveBackward(){
+		driveTrain.move(0.5, 0);
+	}
+		
+			
+    }
+			
+		
+	
+	
+	
+	
+
+
