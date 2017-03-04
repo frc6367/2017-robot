ddd00d375d1732e3aa69f7a4cde93363a7efad9d
Dustin Spicuzza <dustin@virtualroadside.com>
2017-03-04 17:08:27 -0500

    Add @MagicInject to all the autonomous modes

--- src/org/usfirst/frc/team6367/robot/autonomous/SideGearAutonomous.java
+++ src/org/usfirst/frc/team6367/robot/autonomous/SideGearAutonomous.java
@@ -2,10 +2,13 @@ package org.usfirst.frc.team6367.robot.autonomous;
 
 import org.usfirst.frc.team6367.robot.components.DriveTrain;
 
+import io.github.robotpy.magicbot.MagicInject;
 import io.github.robotpy.magicbot.sm.AutonomousStateMachine;
 import io.github.robotpy.magicbot.sm.State;
 
 public class SideGearAutonomous extends AutonomousStateMachine {
+	
+	@MagicInject
 	DriveTrain drivetrain;
 	
 	@State(first = true)
