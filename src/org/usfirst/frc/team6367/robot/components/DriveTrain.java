83cec518c54b924ae160a303bbccef1b74385b22
HKenfack <harold.kenfack@ebrooke.org>
2017-03-04 19:07:49 -0500

    Added code for rotation in DriveTrain

--- src/org/usfirst/frc/team6367/robot/components/DriveTrain.java
+++ src/org/usfirst/frc/team6367/robot/components/DriveTrain.java
@@ -1,80 +1,87 @@
 package org.usfirst.frc.team6367.robot.components;
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import io.github.robotpy.magicbot.MagicComponent;
 
 public class DriveTrain implements MagicComponent {
-	
+
 	double ENCODER_P = 0.1;
-	
+	double ANGLE_P = 0.1;
+
 	// assumes the toughbox is 10.71:1 gear ratio
 	// wheel is 0.5 feet
 	// cimcoder is 20 ticks per revolution
 	static final double ENCODER_K = 10.71*20.0 / (0.5*Math.PI);
-	
+
 	double m_speed = 0;
 	double m_rotation = 0;
-	
-	boolean m_shouldRotate = false;
-	double m_angle = 0;
-	
+
 	RobotDrive m_drive; 
 	Encoder encoder= new Encoder(0,1);
-	
+	AHRS navX = new AHRS(SPI.Port.kMXP);
+
 	@Override
 	public void onEnabled() {
 		ENCODER_P = SmartDashboard.getNumber("encoder_p", ENCODER_P);
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * @return distance traveled in feet
 	 */
-	double getDistance(){
+	public double getDistance(){
 		return encoder.get()*ENCODER_K;
 	}
 	
-		
+	public double getAngle(){
+	 	return navX.getAngle();
+	}
+
+
 	public void move(double speed, double rotation){
 		m_speed = speed;
 		m_rotation = rotation;
 	}
-	
-		
-	
-	
+
+
+
+
 	public boolean moveTo(double dst){
-	
 		double error = (dst - getDistance());
+
 		if(Math.abs(error)<0.15){
 			return true;
-			
+
 		}else{
 			m_speed=ENCODER_P*error;
 			return false;
 		}
 	}
-	
-	public void rotateTo(double angle){
-		m_angle = angle;
-		m_shouldRotate = true;
+
+	public boolean rotateTo(double angle){
+		double error = (angle - getDistance());
+		if(Math.abs(error) < 2.0){
+			return true;
+
+		}else{
+			m_rotation=ANGLE_P*error;
+			return false;
+		}	
 	}
-	
-	public boolean rotateTo(){
-		return false;
-	}
-	
+
 	public void execute(){
 		m_drive.arcadeDrive(m_speed, m_rotation, true);
-		
+
 		// reset the values 
 		m_speed = 0;
 		m_rotation = 0;
-		m_shouldRotate = false;
 	}
-	
+
 
 }
