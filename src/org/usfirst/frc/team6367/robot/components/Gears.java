eb13c5220b95e01a71f1e4c289b5ccfc203e9aa9
Dustin Spicuzza <dustin@virtualroadside.com>
2017-03-13 16:55:20 -0400

    Add initial gears implementation

--- src/org/usfirst/frc/team6367/robot/components/Gears.java
+++ src/org/usfirst/frc/team6367/robot/components/Gears.java
@@ -1,13 +1,34 @@
 package org.usfirst.frc.team6367.robot.components;
 
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import io.github.robotpy.magicbot.MagicComponent;
 
 public class Gears implements MagicComponent {
 
+	boolean gear = false;
+	NetworkTable nt = NetworkTable.getTable("/camera");
+	
+	static final double[] DEFAULT = new double[]{0, 0, 0};
+	
+	public void align() {
+		gear = true;
+	}
+	
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
+		if (gear == true) {
+			
+			// targeting:
+			// target[0] -- is 0 if not found, or 1 if found
+			// target[1] -- time
+			// target[2] -- offset (in degrees) of the target
+			double[] target = nt.getNumberArray("target", DEFAULT);
+			
+			// TODO: do stuff here
+			
+		}
 		
+		gear = false;
 	}
 
 }
