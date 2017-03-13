fd48557e19e095c028714cefe7b88c3b8620b638
HKenfack <harold.kenfack@ebrooke.org>
2017-03-13 17:16:21 -0400

    Fixed code for Fuel

--- src/org/usfirst/frc/team6367/robot/components/Fuel.java
+++ src/org/usfirst/frc/team6367/robot/components/Fuel.java
@@ -1,19 +1,19 @@
 package org.usfirst.frc.team6367.robot.components;
 
-import edu.wpi.first.wpilibj.Victor;
-import io.github.robotpy.magicbot.MagicComponent;
 
+import edu.wpi.first.wpilibj.VictorSP;
+import io.github.robotpy.magicbot.MagicComponent;
 public class Fuel implements MagicComponent {
 
-	static final int FRONT_ON = 1;
+	
 	static final int OFF = 0;
 	static final int REAR_UPPER_ON = 1;
 	static final int REAR_LOWER_IN = 1;
 	static final int REAR_LOWER_OUT =-1;
 	
-	Victor frontIntakeMotor = new Victor(6);
-	Victor upperRearMotor = new Victor(5);
-	Victor rearLowerMotor = new Victor(4);
+	
+	VictorSP upperRearMotor = new VictorSP(5);
+	VictorSP rearLowerMotor = new VictorSP(4);
 	
 
 	boolean shot = false;
@@ -31,26 +31,26 @@ public class Fuel implements MagicComponent {
 
 	public void execute(){
 		if (shot==false && take==false){
-			frontIntakeMotor.set(OFF);
+			
 			upperRearMotor.set(OFF);
 			rearLowerMotor.set(OFF);
 		}
 		else if (shot==true && take==false){
-			frontIntakeMotor.set(OFF);
+			
 			upperRearMotor.set(REAR_UPPER_ON);
 			rearLowerMotor.set(REAR_LOWER_OUT);
 			
 		}
 		
 		else if (shot==false && take==true){
-			frontIntakeMotor.set(FRONT_ON);
+			
 			upperRearMotor.set(REAR_UPPER_ON);
 			rearLowerMotor.set(REAR_LOWER_IN);
 			
 		}
 		
 		else if (shot==true && take==true){
-			frontIntakeMotor.set(FRONT_ON);
+			
 			upperRearMotor.set(REAR_UPPER_ON);
 			rearLowerMotor.set(REAR_LOWER_OUT);
 		}
