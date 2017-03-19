750523ca2ef2862002af489a65726be7a7050c0e
Dustin Spicuzza <dustin@virtualroadside.com>
2017-03-19 00:36:53 -0400

    Cleanup gears code

--- src/org/usfirst/frc/team6367/robot/components/Gears.java
+++ src/org/usfirst/frc/team6367/robot/components/Gears.java
@@ -18,15 +18,15 @@ public class Gears implements MagicComponent {
 	
 	DriveTrain driveTrain;
 	
-	double time;
+	double lastTime;
 	double lastAngle;
-	double foundOrNot;
+	boolean foundOrNot;
 	
 	public Gears(DriveTrain d){
 		driveTrain = d;
-		time = 0.0;
+		lastTime = 0.0;
 		lastAngle = 0.0;
-		foundOrNot = 0.0;
+		foundOrNot = false;
 	}
 	
 	public void align() {
@@ -48,29 +48,30 @@ public class Gears implements MagicComponent {
 			// target[2] -- offset (in degrees) of the target  
 			double[] target = nt.getNumberArray("target", DEFAULT);
 
-			foundOrNot = target[0];
+			// convert the number to a boolean
+			if (target[0] > 0) {
+				foundOrNot = true;
+			} else {
+				foundOrNot = false;
+			}
+			
 			double newTime = target[1];
 			double offset = target[2];
-			// TODO: do stuff here
 			
-			driveTrain.moveTo(lastAngle);
-			
-			if(offset == 0.0){
-				align = false;
-				lastAngle = 0.0;
-				time = 0.0;
-				foundOrNot = 0.0;
-			}
-			else if(newTime!=time && foundOrNot==1.0){ //if I found my target
-				lastAngle = driveTrain.getAngle()-offset;
-				time = newTime;
-			}
-						
+			//if I found my target
+			if (foundOrNot) {
+				
+				// if the time has changed, update the angle
+				if (Math.abs(newTime - lastTime) > 0.001) {
+					lastAngle = driveTrain.getAngle()-offset;
+					lastTime = newTime;
+				}
+				
+				driveTrain.moveTo(lastAngle);
+			}	
 		} else{
 			light.set(Value.kOff);
 		}
-		
-
 		
 		align = false;
 	}
